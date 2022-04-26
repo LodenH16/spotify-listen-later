d54335cceb4ac6ed67920cdd3d0c82612d03ebd7
Thomas Harris <loden16harris@gmail.com>
2022-04-26 17:16:21 -0500

    access spotify through client credentials

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,14 @@
 import * as functions from "firebase-functions";
 import SpotifyWebApi = require("spotify-web-api-node");
 
-const spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken(process.env.TEMPORARY_ACCESS_TOKEN || "");
-
 export const helloWorld = functions.https.onCall(async (data, context) => {
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+  await spotifyApi.clientCredentialsGrant().then((data) => {
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  });
+
   return spotifyApi.searchArtists("Love");
 });
