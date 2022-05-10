aa7af6e8157694d47a0731f190651dd3f050618d
Thomas Harris <loden16harris@gmail.com>
2022-05-09 21:43:02 -0500

    get access tokens with auth code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,9 +18,24 @@ export const createUserFirestoreRecord = functions.auth
 
 // add a user to auth when they login with Spotify
 export const createUserWithSpotify = functions.https.onCall(
-  async ({ token }) => {
-    const spotifyApi = new SpotifyWebApi();
-    spotifyApi.setAccessToken(token);
+  async ({ authCode }) => {
+    let tokenExpirationEpoch;
+    const spotifyApi = new SpotifyWebApi({
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      redirectUri: process.env.REDIRECT_URI,
+    });
+    // access token grant with auth code
+    // https://github.com/thelinmichael/spotify-web-api-node/blob/master/examples/access-token-refresh.js
+    console.log("auth code ***************", authCode);
+    spotifyApi.authorizationCodeGrant(authCode).then((data) => {
+      // set access token and refresh token
+      spotifyApi.setAccessToken(data.body["access_token"]);
+      spotifyApi.setRefreshToken(data.body["refresh_token"]);
+
+      tokenExpirationEpoch =
+        new Date().getTime() / 1000 + data.body["expires_in"];
+    });
 
     const spotifyUser = await spotifyApi.getMe().then((data) => data.body);
 
