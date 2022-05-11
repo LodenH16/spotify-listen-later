bd7cc15c72b620ddbef346b7c707ba57fc7ac96a
Thomas Harris <loden16harris@gmail.com>
2022-05-11 17:30:19 -0500

    add styles for ArtistCard

---
+++ components/ArtistProfile/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ArtistCard = styled.div`
+  border-radius: 12px;
+  background-color: #e8f0fe;
+  padding: 10px;
+  margin: 5px;
+  width: 230px;
+`;
