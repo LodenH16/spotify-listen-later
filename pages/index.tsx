6f8f768a59f69aaa5369963eb88b687941400132
Thomas Harris <loden16harris@gmail.com>
2022-05-12 18:48:17 -0500

    remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { SpotifyAuth, Scopes } from "react-spotify-auth";
 import SpotifyLoginButton from "../components/SpotifyLoginButton/index";
 import ArtistProfile from "../components/ArtistProfile/ArtistProfile";
 import { ArtistCardWrapper } from "../styles/styles";
